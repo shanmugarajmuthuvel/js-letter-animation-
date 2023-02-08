3b5fad04f110dcfee82de6589ad2a74c2c66d3ef
shanmugarajmuthuvel <shanmugarajmuthuvel1777@yahoo.com>
2023-02-08 23:20:56 +0530

    Animation and letters

---
+++ Js Animation Letters/main.js
@@ -0,0 +1,28 @@
+     var container=document.querySelector(".container")
+	 var label=document.querySelector(".animat")
+	 var labell=document.querySelector(".animati")
+// <!------------------------------Normal function method------------------------------------------------>	 
+	       // setTimeout(animation, 3000)
+          // animation()
+	    // function animation(){
+				// Label.textContent="manikharniga"	
+		// }
+		// function animatio(){
+              // Label.textContent=prompt("enter your wife name")
+		// }			  
+ // setTimeout(animatio,3000)
+ // animatio()
+ // <!-----------------------------------------aero function method------------------------------------------------>
+      var letter= ()=>{
+		     setTimeout(()=>{
+				 labell.textContent="poornima";
+			 },2000);
+	  
+			 setTimeout(()=>{
+				 labell.textContent=prompt("enter your wife");
+			 },7000);
+			 setTimeout(()=>{
+				 labell.textContent="pavithra"
+			 },4000);
+	  }
+	  letter()
